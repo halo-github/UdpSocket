aecbe47e2a209358d29a4f440074cbf088221492
halo-github <82198313@qq.com>
2016-06-12 17:14:02 +0800

    Initial Commit

---
+++ UdpSocket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UdpSocket
+//
+//  Created by Fenix on 16/6/12.
+//  Copyright © 2016年 Fenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UdpSocket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UdpSocket
+//
+//  Created by Fenix on 16/6/12.
+//  Copyright © 2016年 Fenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
